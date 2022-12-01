9e9d38976cbd2da8dc03359cb5799ff6a24f5ead
Funt <funt_88@mail.ru>
2022-12-01 22:54:59 +0300

    task_2 completed

---
+++ Task_2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+int b1 = ReadInt("введите значение b1: ");
+int k1 = ReadInt("введите значение k1: ");
+int b2 = ReadInt("введите значение b2: ");
+int k2 = ReadInt("введите значение k2: ");
+if (k1 == k2)
+{
+    Console.WriteLine("Прямые параллельны!");
+}
+else
+{
+    int x = (b2 - b1) / (k1 - k2);
+    int y = k1 * x + b1;
+    Console.WriteLine($"Две прямые пересекутся в X: {x}, Y: {y}");
+}
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
